c98f2e675f667e7b129c5d0c0891a40bc4c1c86c
SemirHajdar <98660616+SemirHajdar@users.noreply.github.com>
2022-03-23 10:48:44 +0100

    Update RunnerForStudent.java

--- RunnerForStudent.java
+++ RunnerForStudent.java
@@ -4,16 +4,13 @@ import java.util.HashMap;
 
 public class RunnerForStudent {
     public static void main(String[] args) {
-        HashMap<Integer,String> studentMap = new HashMap<>();
-        studentMap.put(1,"Amir");
-        studentMap.put(2,"Damir");
-        studentMap.put(3,"Mujo");
-        studentMap.put(4,"Suljo");
+        HashMap<Integer, String> studentMap = new HashMap<>();
+        studentMap.put(1, "Amir");
+        studentMap.put(2, "Damir");
+        studentMap.put(3, "Mujo");
+        studentMap.put(4, "Suljo");
         Student myStudent = new Student(studentMap);
-
+        myStudent.removeStudent();
         // use this Runner to manipulate with Student Class
-
-
-
     }
 }
