845970f8604d11ef7886ded2322a22634528c37e
SemirHajdar <98660616+SemirHajdar@users.noreply.github.com>
2022-03-23 10:47:50 +0100

    Update RunnerForCoffeeMachine.java

--- RunnerForCoffeeMachine.java
+++ RunnerForCoffeeMachine.java
@@ -3,9 +3,7 @@ package oopTwo;
 public class RunnerForCoffeeMachine {
     public static void main(String[] args) {
 
-        CoffeeMachine myCoffeeMachine = new CoffeeMachine(0.00,0.00, 0.00);
-
-        myCoffeeMachine.alertForLowCapacityOfCoffe();
-
+        CoffeeMachine myCoffeeMachine = new CoffeeMachine(0.00, 56.00, 100.00);
+        myCoffeeMachine.alertForLowCapacityOfCoffee();
     }
 }
